0a78fa1bdaf315e7d4c4a9079b98ea934ea341e3
RebecaConte <becaconte@gmail.com>
2021-04-26 17:38:17 +0100

    route for new group done

--- routes/groups.routes.js
+++ routes/groups.routes.js
@@ -1,32 +1,32 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const Group = require("../models/Group.model");
+const User = require("../models/User.model");
 
 /* GET /create group  */
-router.get("/profiles/:userId/groups/new", (req, res, next) => {
+router.get("/profile/groups/new", (req, res, next) => {
   res.render("groups/new.hbs");
 });
 
 
 /* POST /new group */
-router.post("/profiles/:userId/groups/create", (req, res, next) => {
-  const { groupName, image, description } = req.body;
+router.post("/profile/groups/create", (req, res, next) => {
+  const user = req.session.currentUser;
+  const {groupName, image, description, members, movies} = req.body
 
-  Group.findOne({ groupName }).then((group) => {
+  Group.findOne({groupName})
+  .then((group) => {
+    console.log(group);
+
     if (group) {
       res.render("groups/new.hbs", { msg: "Group Name already taken" });
       return;
     }
 
-  Group.create({ groupName, image, description })
-    .then((group) => {
-      res.redirect(`/groups/${group._id}`)
-    })
+  Group.create({ groupName, image, description})
+    .then((group) => res.redirect("/profile"))
     .catch((err) => next(err));
   });
 });
-
-
-
 
 module.exports = router;
