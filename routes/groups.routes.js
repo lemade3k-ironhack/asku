f1924438889432c1bb823c788874077d1889d102
lemade <made@honkgong.info>
2021-04-28 14:53:54 +0200

    Update members and refactor

--- routes/groups.routes.js
+++ routes/groups.routes.js
@@ -8,37 +8,24 @@ const { authorize, authMember } = require("../middlewares/authorization");
 const validate = require("../middlewares/validations/groups")
 const uploader = require("../middlewares/cloudinary.config");
 
+// require helpers
+const updateMembers = require("../helpers/groups.helpers")
+
 /* GET /groups/new  */
 router.get("/groups/new", authorize, (req, res) => {
   res.render("groups/new.hbs");
 });
 
 /* POST /groups/create */
-router.post("/groups/create", authorize, uploader.single("image"), validate, (req, res, next) => {
+router.post("/groups/create", authorize, uploader.single("image"), validate, async (req, res, next) => {
   const user = req.session.currentUser;
   const { groupName, image, description } = req.body;
   const newImg = (req.file != undefined) ? req.file.path : image;
-
-  // add members to group
-  // ToDo: move to middleware
-  let members = req.body.members.split(', ')
-  let groupMembers = [user._id]
-
-  members.forEach(member => {
-    User.findOne({username: member})
-      .then(resUser => {
-        if (resUser.username != user.username) {
-          groupMembers.push(resUser.id)
-        }
-        return;
-      })
-      .catch(() => {
-        console.log("User not found")
-        return;
-      })
-  })
+  // wait for helper functions promises to finish before returning something
+  const groupMembers = await updateMembers(members, user)
 
   Group.findOne({ groupName }).then((group) => {
+    // if groupname already exists render form with message 
     if (group) {
       res.render("groups/new.hbs", {
         groupName, description,
@@ -60,17 +47,32 @@ router.post("/groups/create", authorize, uploader.single("image"), validate, (re
 /* GET /groups/:groupId/edit  */
 router.get("/groups/:groupId/edit", authorize, authMember, (req, res, next) => {
   const groupId = req.params.groupId;
+  const currentUser = req.session.currentUser
 
   Group.findById( groupId )
-    .then((group) => res.render("groups/edit.hbs", { group }))
+    .populate("users")
+    .then((group) => {
+      let withoutSelf = group.users.filter(user => {
+        return user._id != currentUser._id
+      })
+      // display all group members except current user
+      group.members = withoutSelf.map(user => user.username).join(', ')
+      
+      res.render("groups/edit.hbs", { group })
+    })
     .catch((err) => next(err));
 });
 
 /* POST /groups/:groupId/update  */
-router.post("/groups/:groupId/update", authorize, authMember, uploader.single("image"), validate, (req, res, next) => {
+router.post(
+  "/groups/:groupId/update", authorize, authMember, 
+  uploader.single("image"), validate, async (req, res, next) => {
+  const user = req.session.currentUser
   const groupId = req.params.groupId
   const { groupName, image, description } = req.body;
   const newImg = (req.file != undefined) ? req.file.path : image;
+  // wait for helper functions promises to finish before returning something
+  const groupMembers = await updateMembers(members, user)
 
   Group.findByIdAndUpdate(groupId, { groupName, image: newImg, description })
     .then(() => { res.redirect("/groups/" + groupId)})
