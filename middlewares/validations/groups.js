9f1dcd8e8e431aa84244cdba1d77e5fb8d2b1100
lemade <made@honkgong.info>
2021-04-29 11:50:05 +0200

    Fix redirect after validation error on groups

--- middlewares/validations/groups.js
+++ middlewares/validations/groups.js
@@ -1,11 +1,12 @@
 /* validate user input on group create */
 const validate = (req, res, next) => {
   const group = req.body;
+  group._id = req.params.groupId
   group.image = (req.file != undefined) ? req.file.path : req.body.oldImg;
   const path = req.route.path.match("create") ? "groups/new.hbs" : "groups/edit.hbs";
 
   if (!group.groupName) {
-    res.render(path, { group, msg: "Please add a name for your group!" });
+    res.render(path, { groupId: group._id, group, msg: "Please add a name for your group!" });
   } else {
     next();
   }
